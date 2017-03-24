10112b2d7a9b3223e80c30027d1ae9109c4939ca
scofield-hello <scofield.hello@gmail.com>
2017-03-24 17:40:22 +0800

    添加非定时录音方法

--- cordova-plugin-sound-recorder/src/android/SoundRecorder.java
+++ cordova-plugin-sound-recorder/src/android/SoundRecorder.java
@@ -13,6 +13,7 @@ import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
 import org.apache.cordova.LOG;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -30,8 +31,12 @@ import java.io.IOException;
 public class SoundRecorder extends CordovaPlugin {
   private static final String TAG = "SoundRecorder";
 
-  private final static int PERMISSION_REQUEST_CODE = 1001;
+  private final static int REQUEST_COUNTDOWN = 1001;
+  private final static int REQUEST_MANUAL= 1002;
+
   private final static int DEFAULT_DURATION = 10;//默认录制时间 单位秒
+  private final static int MODE_COUNTDOWN = 0;
+  private final static int MODE_MANUAL = 1;
   private final static String AUDIO_DIR = "AudioRecords";
   private final static String DATE_FORMAT = "yyyyMMddHHmmss";
   private final static String RAW_SUFFIX = ".raw";
@@ -47,6 +52,8 @@ public class SoundRecorder extends CordovaPlugin {
   private AudioRecord audioRecord;
   private int duration = DEFAULT_DURATION;
   private boolean isRecording = false;
+  private String wavFilename,rawFilename;
+  private int mode = MODE_COUNTDOWN;
   private final static String[] requiredPermissions = new String[]{
     Manifest.permission.RECORD_AUDIO,
     Manifest.permission.WRITE_EXTERNAL_STORAGE,
@@ -56,21 +63,249 @@ public class SoundRecorder extends CordovaPlugin {
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
     this.callbackContext = callbackContext;
-    if (action.equals("record")) {
+    boolean actionResult = false;
+    if (action.equals("countdownRecord")) {
+      actionResult = true;
       config = args.getJSONObject(0);
-      if (cordova.hasPermission(requiredPermissions[0])
-        && cordova.hasPermission(requiredPermissions[1])
-        && cordova.hasPermission(requiredPermissions[2])){
-        this.startRecord(config, callbackContext);
+      if (checkRequiredPermission()){
+        this.countdownRecord(config, callbackContext);
       }else{
-          cordova.requestPermissions(this,PERMISSION_REQUEST_CODE, requiredPermissions);
+          mode = MODE_COUNTDOWN;
+          cordova.requestPermissions(this, REQUEST_COUNTDOWN, requiredPermissions);
       }
+      return actionResult;
+    }
+    if (action.equals("manualRecord")){
+      actionResult = true;
+      if (checkRequiredPermission()){
+        this.manualRecord(callbackContext);
+      }else{
+        mode = MODE_MANUAL;
+        cordova.requestPermissions(this, REQUEST_MANUAL, requiredPermissions);
+      }
+      return actionResult;
+    }
+    if (action.equals("manualStop")){
+      actionResult = true;
+      this.manualStop(callbackContext);
+    }
+    return actionResult;
+  }
+
+  private boolean checkRequiredPermission(){
+    return (cordova.hasPermission(requiredPermissions[0])
+      && cordova.hasPermission(requiredPermissions[1])
+      && cordova.hasPermission(requiredPermissions[2]));
+  }
+
+
+  /**
+   * 生成文件存放目录，初始化文件路径
+   * @return true: 初始化成功，false: 初始化失败
+   */
+  private boolean initAudioFilenames(){
+    String filename = DateFormat.format(DATE_FORMAT, System.currentTimeMillis()).toString();
+    String directoryPath = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + AUDIO_DIR;
+    File directory = new File(directoryPath);
+    if (!directory.exists()) {
+      if (!directory.mkdirs()) {
+        LOG.e(TAG, "文件夹创建失败");
+        return false;
+      }
+    }
+    rawFilename = directoryPath + File.separator + filename + RAW_SUFFIX;
+    wavFilename = directoryPath + File.separator + filename + WAV_SUFFIX;
+    return true;
+  }
+
+
+  private boolean startRecording(){
+    if (!isRecording){
+      int audioSource = MediaRecorder.AudioSource.MIC;
+      audioRecord = new AudioRecord(audioSource, simpleRate, chanel, encoding, bufferSize);
+      audioRecord.startRecording();
+      isRecording = true;
       return true;
     }
     return false;
   }
 
-  private void monitorAutoStop(final String rawFilename, final String wavFilename){
+  /**
+   * 实时处理音频文件，从RAW格式转换为WAV格式
+   */
+  private void writeWavFileInRealtime(){
+    cordova.getThreadPool().execute(new Runnable() {
+      @Override
+      public void run() {
+        LOG.d(TAG, "录音程序正在运行中....");
+        byte[] soundBytes = new byte[bufferSize];
+        FileOutputStream fileOutputStream;
+        int read;
+        File rawAudioFile = new File(rawFilename);
+        if (rawAudioFile.exists()) {
+          if (!rawAudioFile.delete()) {
+            stopRecord();
+            LOG.e(TAG, "处理音频文件出错,旧文件无法删除");
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.ERROR, "处理音频文件出错,旧文件找不到.");
+            callbackContext.sendPluginResult(pluginResult);
+            return;
+          }
+        }
+        try {
+          fileOutputStream = new FileOutputStream(rawAudioFile);
+        } catch (FileNotFoundException e) {
+          stopRecord();
+          LOG.e(TAG, "处理音频文件出错,文件找不到", e);
+          PluginResult pluginResult = new PluginResult(PluginResult.Status.ERROR, "处理音频文件出错,文件找不到.");
+          callbackContext.sendPluginResult(pluginResult);
+          return;
+        }
+        while (isRecording) {
+          read = audioRecord.read(soundBytes, 0, bufferSize);
+          if (AudioRecord.ERROR_INVALID_OPERATION != read) {
+            try {
+              fileOutputStream.write(soundBytes);
+            } catch (IOException e) {
+              stopRecord();
+              LOG.e(TAG, "处理音频文件出错,写入时异常", e);
+              PluginResult pluginResult = new PluginResult(PluginResult.Status.ERROR, "写入音频文件异常.");
+              callbackContext.sendPluginResult(pluginResult);
+              return;
+            }
+          }
+        }
+        try {
+          fileOutputStream.close();
+        } catch (IOException e) {
+          stopRecord();
+          LOG.e(TAG, "处理音频文件出错,关闭输出流时异常", e);
+          PluginResult pluginResult = new PluginResult(PluginResult.Status.ERROR, "输出流关闭异常.");
+          callbackContext.sendPluginResult(pluginResult);
+          return;
+        }
+        long byteRate = 16 * simpleRate * chanel / 8;
+        byte[] destData = new byte[bufferSize];
+        try {
+          FileInputStream fileInputStream = new FileInputStream(rawAudioFile);
+          fileOutputStream = new FileOutputStream(wavFilename);
+          long srcLength = fileInputStream.getChannel().size();
+          long destLength = srcLength + 36;
+          writeHeaderData(fileOutputStream, srcLength, destLength, simpleRate, chanel, byteRate);
+          while (fileInputStream.read(destData) != -1) {
+            fileOutputStream.write(destData);
+          }
+          fileInputStream.close();
+          fileOutputStream.close();
+        } catch (IOException e) {
+          stopRecord();
+          LOG.e(TAG, "处理音频文件出错,格式转换时异常", e);
+          PluginResult pluginResult = new PluginResult(PluginResult.Status.ERROR, "转换WAV格式异常.");
+          callbackContext.sendPluginResult(pluginResult);
+        }
+      }
+    });
+  }
+
+  /**
+   * 倒计时录音
+   * @param config 配置信息
+   * @param callbackContext 回调
+   * @throws JSONException JSON异常
+   */
+  private void countdownRecord(JSONObject config, final CallbackContext callbackContext) throws  JSONException {
+    mode = MODE_COUNTDOWN;
+    duration = (config.has("duration") ? config.getInt("duration") : DEFAULT_DURATION);
+    if (duration < 1) {
+      PluginResult pluginResult = new PluginResult(PluginResult.Status.ERROR, "参数配置错误,录音时间最短为1秒.");
+      callbackContext.sendPluginResult(pluginResult);
+    } else {
+      if (!initAudioFilenames()){
+        PluginResult pluginResult = new PluginResult(PluginResult.Status.ERROR, "初始化文件地址失败.");
+        callbackContext.sendPluginResult(pluginResult);
+        return;
+      }
+      if (!startRecording()){
+        PluginResult pluginResult = new PluginResult(PluginResult.Status.ERROR, "录音机实例初始化失败或已在录音中.");
+        callbackContext.sendPluginResult(pluginResult);
+        return;
+      }
+      monitorCountdownRecord();
+      writeWavFileInRealtime();
+    }
+  }
+
+  /**
+   * 手动开始录音
+   * @param callbackContext 回调
+   */
+  private void manualRecord(final CallbackContext callbackContext){
+    duration = 0;
+    mode = MODE_MANUAL;
+    if (!initAudioFilenames()){
+      PluginResult pluginResult = new PluginResult(PluginResult.Status.ERROR, "初始化文件地址失败.");
+      callbackContext.sendPluginResult(pluginResult);
+      return;
+    }
+    if (!startRecording()){
+      PluginResult pluginResult = new PluginResult(PluginResult.Status.ERROR, "录音机实例初始化失败或已在录音中.");
+      callbackContext.sendPluginResult(pluginResult);
+    }else{
+      PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
+      result.setKeepCallback(true);// 保证持久回调
+      callbackContext.sendPluginResult(result);
+      monitorManualRecord();
+      writeWavFileInRealtime();
+    }
+  }
+
+  /**
+   * 手动停止录音
+   * @param callbackContext 回调
+   * @throws JSONException JSON异常
+   */
+  private void manualStop(final CallbackContext callbackContext) throws JSONException{
+    if (isRecording){
+      stopRecord();
+      JSONObject jsonObject = new JSONObject();
+      jsonObject.put("uri", Uri.fromFile(new File(wavFilename)));
+      PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, jsonObject);
+      callbackContext.sendPluginResult(pluginResult);
+      File originalRawFile = new File(rawFilename);
+      if (originalRawFile.exists()){
+        boolean result = originalRawFile.delete();
+        LOG.d(TAG, "原始RAW录音文件删除" + (result ? "成功":"失败"));
+      }
+    }else{
+      PluginResult pluginResult = new PluginResult(PluginResult.Status.ERROR, "录音已经停止.");
+      callbackContext.sendPluginResult(pluginResult);
+    }
+  }
+
+
+  private void monitorManualRecord(){
+    cordova.getThreadPool().execute(new Runnable() {
+      @Override
+      public void run() {
+       while (isRecording){
+         try {
+           Thread.sleep(1000);
+           duration ++;
+           LOG.d(TAG, "已录音时长" + duration+ "秒");
+           PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, duration);
+           pluginResult.setKeepCallback(true);
+           callbackContext.sendPluginResult(pluginResult);
+         } catch (InterruptedException e) {
+           stopRecord();
+           LOG.e(TAG, "录音过程中出现线程异常", e);
+           PluginResult pluginResult = new PluginResult(PluginResult.Status.ERROR, "录音过程中出现线程异常.");
+           callbackContext.sendPluginResult(pluginResult);
+         }
+       }
+      }
+    });
+  }
+
+  private void monitorCountdownRecord(){
     cordova.getThreadPool().execute(new Runnable() {
       @Override
       public void run() {
@@ -83,7 +318,8 @@ public class SoundRecorder extends CordovaPlugin {
               stopRecord();
               JSONObject jsonObject = new JSONObject();
               jsonObject.put("uri", Uri.fromFile(new File(wavFilename)));
-              callbackContext.success(jsonObject);
+              PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, jsonObject);
+              callbackContext.sendPluginResult(pluginResult);
               File originalRawFile = new File(rawFilename);
               if (originalRawFile.exists()){
                 boolean result = originalRawFile.delete();
@@ -93,7 +329,8 @@ public class SoundRecorder extends CordovaPlugin {
           } catch (Exception e) {
             stopRecord();
             LOG.e(TAG, "录音过程中出现线程异常问题", e);
-            callbackContext.error("录音过程中出现线程异常问题.");
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.ERROR, "录音过程中出现线程异常问题.");
+            callbackContext.sendPluginResult(pluginResult);
           }
         }
       }
@@ -105,97 +342,8 @@ public class SoundRecorder extends CordovaPlugin {
     audioRecord.stop();
     audioRecord.release();
     audioRecord = null;
+    duration = 0;
     LOG.d(TAG, "录音程序已停止.");
-  }
-
-  private void startRecord(JSONObject config, final CallbackContext callbackContext) throws  JSONException {
-    duration = (config.has("duration") ? config.getInt("duration") : DEFAULT_DURATION);
-    if (duration < 1) {
-      callbackContext.error("参数配置错误,录音时间最短为1秒.");
-    } else {
-      String filename = DateFormat.format(DATE_FORMAT, System.currentTimeMillis()).toString();
-      String directoryPath = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + AUDIO_DIR;
-      File directory = new File(directoryPath);
-      if (! directory.exists()){
-        if (!directory.mkdirs()){
-          LOG.e(TAG, "文件夹创建失败");
-          callbackContext.error("文件夹创建失败.");
-          return;
-        }
-      }
-      final String rawFilename = directoryPath + File.separator  + filename + RAW_SUFFIX;
-      final String wavFilename = directoryPath + File.separator  + filename + WAV_SUFFIX;
-      int audioSource = MediaRecorder.AudioSource.MIC;
-      audioRecord = new AudioRecord(audioSource, simpleRate, chanel, encoding, bufferSize);
-      audioRecord.startRecording();
-      isRecording = true;
-      monitorAutoStop(rawFilename, wavFilename);
-      cordova.getThreadPool().execute(new Runnable() {
-        @Override
-        public void run() {
-          LOG.d(TAG, "录音程序正在运行中....");
-          byte[] soundBytes = new byte[bufferSize];
-          FileOutputStream fileOutputStream;
-          int read;
-          File rawAudioFile = new File(rawFilename);
-          if (rawAudioFile.exists()){
-            if (!rawAudioFile.delete()){
-              stopRecord();
-              LOG.e(TAG, "处理音频文件出错,旧文件无法删除");
-              callbackContext.error("处理音频文件出错,旧文件找不到.");
-              return;
-            }
-          }
-          try{
-            fileOutputStream = new FileOutputStream(rawAudioFile);
-          } catch (FileNotFoundException e){
-            stopRecord();
-            LOG.e(TAG, "处理音频文件出错,文件找不到", e);
-            callbackContext.error("处理音频文件出错,文件找不到.");
-            return;
-          }
-          while(isRecording){
-            read = audioRecord.read(soundBytes, 0, bufferSize);
-            if (AudioRecord.ERROR_INVALID_OPERATION != read){
-              try {
-                fileOutputStream.write(soundBytes);
-              } catch (IOException e) {
-                stopRecord();
-                LOG.e(TAG, "处理音频文件出错,写入时异常", e);
-                callbackContext.error("处理音频文件出错,写入时异常.");
-                return;
-              }
-            }
-          }
-          try {
-            fileOutputStream.close();
-          } catch (IOException e) {
-            stopRecord();
-            LOG.e(TAG, "处理音频文件出错,关闭输出流时异常", e);
-            callbackContext.error("处理音频文件出错,关闭输出流时异常.");
-            return;
-          }
-          long byteRate = 16 * simpleRate * chanel /8;
-          byte[] destData = new byte[bufferSize];
-          try{
-            FileInputStream fileInputStream = new FileInputStream(rawAudioFile);
-            fileOutputStream = new FileOutputStream(wavFilename);
-            long srcLength = fileInputStream.getChannel().size();
-            long destLength = srcLength + 36;
-            writeHeaderData(fileOutputStream, srcLength, destLength, simpleRate, chanel, byteRate);
-            while(fileInputStream.read(destData) != -1){
-              fileOutputStream.write(destData);
-            }
-            fileInputStream.close();
-            fileOutputStream.close();
-          }catch (IOException e){
-            stopRecord();
-            LOG.e(TAG, "处理音频文件出错,格式转换时异常", e);
-            callbackContext.error("处理音频文件出错,格式转换时异常.");
-          }
-        }
-      });
-    }
   }
 
   private void writeHeaderData(FileOutputStream fileOutputStream, long srcLength,long destLength, long sampleRate, int channel, long byteRate) throws IOException{
@@ -252,10 +400,20 @@ public class SoundRecorder extends CordovaPlugin {
     for (int grantResult : grantResults) {
       if (grantResult != PackageManager.PERMISSION_GRANTED){
         LOG.e(TAG, "请先授予应用所需权限");
-        callbackContext.error("请先授予应用所需权限.");
+        PluginResult pluginResult = new PluginResult(PluginResult.Status.ERROR, "请先授予应用所需权限.");
+        callbackContext.sendPluginResult(pluginResult);
         return;
       }
     }
-    startRecord(config,callbackContext);
+    switch (mode){
+      case MODE_COUNTDOWN:
+        countdownRecord(config, callbackContext);
+        break;
+      case MODE_MANUAL:
+        manualStop(callbackContext);
+        break;
+      default:
+        break;
+    }
   }
 }
--- cordova-plugin-sound-recorder/www/SoundRecorder.js
+++ cordova-plugin-sound-recorder/www/SoundRecorder.js
@@ -1,10 +1,17 @@
 var exec = require('cordova/exec');
 var SoundRecorder = function(){};
 
-SoundRecorder.prototype.record = function(arg0, success, error) {
-    exec(success, error, "SoundRecorder", "record", [arg0]);
+SoundRecorder.prototype.countdownRecord = function(arg0, success, error) {
+    exec(success, error, "SoundRecorder", "countdownRecord", [arg0]);
 };
 
+SoundRecorder.prototype.manualRecord = function(success, error) {
+    exec(success, error, "SoundRecorder", "manualRecord", []);
+};
+
+SoundRecorder.prototype.manualStop = function(success, error) {
+    exec(success, error, "SoundRecorder", "manualStop", []);
+};
 
 var recorder = new SoundRecorder();
 module.exports = recorder;
